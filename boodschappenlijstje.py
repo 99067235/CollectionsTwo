ab2a3924452de83b8c8ac22d286a909f8205969a
unknown <99067235@mydavinci.nl>
2022-01-11 11:02:57 +0100

    set met kaarten toegevoegd'

--- boodschappenlijstje.py
+++ boodschappenlijstje.py
@@ -1,4 +1,25 @@
 lijstje = {}
 
-add = input("Wat wil je aan het lijstje toevoegen? ")
-if add = ``````````````
+def snapNiet():
+    print("Sorry, dat snap ik niet... ")
+
+def add():
+    toevoegen = input("Wat wil je aan het lijstje toevoegen? ").lower()
+    if toevoegen in lijstje:
+        lijstje[toevoegen] += 1
+    else:
+        lijstje[toevoegen] = 1
+    meer = input("Wil je nog meer bestellen? ").upper()
+    if meer == "JA" or meer  ==  "J":
+        add()
+    elif meer == "NEE" or meer == "N":
+        print("Bedankt voor uw bestelling! ")
+        print(lijstje)
+        exit()
+    else:
+        snapNiet()
+        add()
+    return lijstje
+    
+add()
+print(lijstje)
---
+++ set met kaarten.py
@@ -0,0 +1,20 @@
+import random
+
+bestaandeKleuren = ['harten', 'klaveren', 'schoppen', 'ruiten', 'joker']
+soorten = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "boer", "vrouw", "heer", "aas"]
+deck = []
+jokers = 2
+
+for i in range(47):
+    cardColor = random.choice(bestaandeKleuren)
+    cardNumber = random.choice(soorten)
+    if cardColor == "joker" and jokers > 0:
+        jokers -= 1
+        deck.append(cardColor)
+    elif cardColor == 'joker' and jokers == 0:
+        i -= 1
+    else:
+        toevoegen = cardColor, cardNumber
+        deck.append(toevoegen)
+
+print(deck)
