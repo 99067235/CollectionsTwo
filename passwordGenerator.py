7703d1e877fa0d13dfbc065acd100f0e7d62e0da
unknown <99067235@mydavinci.nl>
2022-01-20 15:41:17 +0100

    specialetekens enzo toegevoegd

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -4,6 +4,7 @@ upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
 lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 specialCharacters = ['@', '#', '$', '%', '&', '_', '?']
+extraList = ['upperCaseLetters', 'lowerCaseLetters', 'numbers', 'specialCharacters' ]
 password = []
 kleineletters = 0
 hoofdletters = 0
@@ -14,19 +15,48 @@ def choose():
     global hoofdletters
     global kleineletters
     global i
-    while i < 24:
-        choose = random.randint(1,2)
-        if choose == 1:
-            choose1 =  random.choice(upperCaseLetters)
+    for l in range(3):
+        choice = random.randint(1,3)
+        if choice == 1 and kleineletters <8:
+            lcChoice = random.choice(lowerCaseLetters)
+            kleineletters += 1
+            password.append(lcChoice)
+        elif choice == 2 and hoofdletters <6:
+            ucChoice = random.choice(upperCaseLetters)
             hoofdletters += 1
-            password.append(choose1)
+            password.append(ucChoice)
+        elif choice == 3 and l > 1:
+            scChoice = random.choice(specialCharacters)
+            password.append(scChoice)
+    chooseLetters()
+
+def chooseLetters():
+    global hoofdletters
+    global kleineletters
+    global nummers
+    global specialeTekens
+    for i in range(21):
+        choice2 = random.randint(1,4)
+        if choice2 == 1:
+            ucChoice2 =  random.choice(upperCaseLetters)
+            hoofdletters += 1
+            password.append(ucChoice2)
             i = i + 1
-        
-        else:
-            choose2 = random.choice(lowerCaseLetters)
+        elif choice2 == 2:
+            lcChoice2 = random.choice(lowerCaseLetters)
             listToStringWithoutBrackets(password)
             kleineletters += 1
-            password.append(choose2)
+            password.append(lcChoice2)
+            i = i + 1
+        elif choice2 == 3:
+            numberchoice = random.choice(numbers)
+            password.append(numberchoice)
+            nummers += 1
+            i = i + 1
+        elif choice2 == 4 and i < 21 and specialeTekens <= 3:
+            specialCharacterChoice = random.choice(specialCharacters)
+            password.append(specialCharacterChoice)
+            specialeTekens += 1
             i = i + 1
     print(listToStringWithoutBrackets(password))
     
